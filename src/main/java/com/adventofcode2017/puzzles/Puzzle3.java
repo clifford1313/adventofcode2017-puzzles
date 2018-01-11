9f01ef927591abf090ad0090904b64ff2a50b712
jfgrezes <jfgrezes@sciforma.com>
2018-01-11 15:41:26 +0100

    Puzzle 3

--- src/main/java/com/adventofcode2017/puzzles/Puzzle3.java
+++ src/main/java/com/adventofcode2017/puzzles/Puzzle3.java
@@ -1,10 +1,32 @@
 package com.adventofcode2017.puzzles;
 
+import java.util.HashMap;
+
 import com.adventofcode2017.IPuzzle;
 
 public class Puzzle3 implements IPuzzle {
+	private final int INPUT = 312051;
+	private HashMap<Node, Integer> nodes = new HashMap<Node, Integer>();
 	@Override
 	public void execute() {
-		System.out.println("AHAHA");
+		int cur = 1;
+		int curx = 0;
+		int cury = 0;
+		while(cur <= INPUT) {
+			if(nodes.containsKey(new Node(curx, cury))) {
+				
+			}
+		}
+	}
+	
+	
+	private class Node {
+		int x;
+		int y;
+		
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
 	}
 }
