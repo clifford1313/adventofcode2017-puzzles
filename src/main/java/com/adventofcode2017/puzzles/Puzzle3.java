c74254732313405713c0a7932c1e1bf40ec226d1
jfgrezes <jfgrezes@sciforma.com>
2018-03-23 11:41:39 +0100

    Puzzle 3 part 1 resolved

--- src/main/java/com/adventofcode2017/puzzles/Puzzle3.java
+++ src/main/java/com/adventofcode2017/puzzles/Puzzle3.java
@@ -6,27 +6,36 @@ import com.adventofcode2017.IPuzzle;
 
 public class Puzzle3 implements IPuzzle {
 	private final int INPUT = 312051;
-	private HashMap<Node, Integer> nodes = new HashMap<Node, Integer>();
+//	private final int INPUT = 37;
 	@Override
 	public void execute() {
-		int cur = 1;
-		int curx = 0;
-		int cury = 0;
-		while(cur <= INPUT) {
-			if(nodes.containsKey(new Node(curx, cury))) {
-				
+		//determine which square is the good one
+		int squarenb = 1;
+		int squaresize = 0;
+		for(int i = 2;;i+=2) {
+			if(i * i > INPUT) {
+				break;
 			}
+			squarenb++;
+			squaresize = i;
 		}
-	}
-	
-	
-	private class Node {
-		int x;
-		int y;
 		
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
+		System.out.println("Square nb = " + squarenb + " - Square size = " + squaresize);
+		
+		//determine position of INPUT in the square
+		int sc1 = squaresize * squaresize;
+		int sc2 = 0;
+		int inputpos = 0;
+		for(int i = 0; i < 4; i++) {
+			sc2 = sc1 + squaresize;
+			if(sc1 < INPUT && sc2 > INPUT) {
+				int mid = (sc1 + sc2) / 2;
+				inputpos = Math.abs(INPUT - mid);
+				break;
+			}
+			sc1 = sc2;
 		}
+		
+		System.out.println("Result = " + (inputpos + squarenb));
 	}
 }
