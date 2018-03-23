02c88d9ed7e92fed12cc70dabfbeceee22b76bae
jfgrezes <jfgrezes@sciforma.com>
2018-03-23 14:28:09 +0100

    New Puzzle 4

---
+++ src/main/java/com/adventofcode2017/puzzles/Puzzle4.java
@@ -0,0 +1,23 @@
+package com.adventofcode2017.puzzles;
+
+import java.util.HashMap;
+
+import com.adventofcode2017.IPuzzle;
+
+public class Puzzle4 implements IPuzzle {
+	private final int INPUT = 312051;
+//	private final int INPUT = 37;
+	@Override
+	public void execute() {
+		executeStep1();
+		executeStep2();
+	}
+	
+	private void executeStep2() {	
+	}
+	
+
+	
+	private void executeStep1() {
+	}
+}
